fde2e8b8eab04bfeac4d4d8a35e8e8c64dfd1fda
Akshay Kumar R <122593218+Akshay0112@users.noreply.github.com>
2025-01-06 22:23:29 +0530

    Add files via upload

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+  bookTitle: string ="";
+  bookAurthor: string ="";
+  
+
+
+
+
+
+
+}
